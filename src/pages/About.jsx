265eb7b0da526693c322ff257d086b779d0af065
Anthony <anthonypoullain.pro@gmail.com>
2022-09-15 17:13:31 +0200

    :recycle: Refactor about page data fetch

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import './About.css';
 import Hero from '../components/Hero/Hero';
 import Dropdown from '../components/Dropdown/Dropdown';
@@ -5,22 +6,32 @@ import heroImg from '../assets/about_hero_img.png';
 import getAboutInfo from '../api/getAboutInfo';
 
 function About() {
-  const data = getAboutInfo();
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getAboutInfo();
+      setData(result);
+    };
+    fetchData();
+  }, []);
 
   return (
-    <>
-      <Hero img={heroImg} />
-      <div className="dropdown-section">
-        {data.map((item) => (
-          <Dropdown
-            type="about"
-            key={item.key}
-            text={item.title}
-            content={item.content}
-          />
-        ))}
-      </div>
-    </>
+    data && (
+      <>
+        <Hero img={heroImg} />
+        <div className="dropdown-section">
+          {data.map((item) => (
+            <Dropdown
+              type="about"
+              key={item.key}
+              text={item.title}
+              content={item.content}
+            />
+          ))}
+        </div>
+      </>
+    )
   );
 }
 
