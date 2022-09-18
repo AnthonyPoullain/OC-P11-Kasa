4f78a7877bd0292fe3119a38b743e33cffffd9b7
Anthony <anthonypoullain.pro@gmail.com>
2022-09-18 10:47:46 +0200

    :memo: Add helper function documentation

--- src/helpers/helperFunctions.js
+++ src/helpers/helperFunctions.js
@@ -1,3 +1,12 @@
+/**
+ * Change the current tab title.
+ * The suffix ' - Kasa' is automatically added.
+ *
+ * @param {string} newTitle - New tab title
+ *
+ * @example
+ * 		tabTitle('New tab title here');
+ */
 export default function tabTitle(newTitle) {
   if (!newTitle) return;
   document.title = `${newTitle} - Kasa`;
