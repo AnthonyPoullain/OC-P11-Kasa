f7756dc470f2ae053f87e86a3b1f1ee42ed76c4c
Anthony <anthonypoullain.pro@gmail.com>
2022-10-24 11:29:42 +0700

    :sparkles: Add lazy loading to Slider component images

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,10 +1,33 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import leftArrow from '../../assets/slideshow_left-arrow.svg';
 import rightArrow from '../../assets/slideshow_right-arrow.svg';
 import './Slider.css';
 
 function Slider({ title, images }) {
 	const [count, setCount] = useState(0);
+
+	// Track loading of the first image + all images for lazyloading
+	const [firstImgLoaded, setFirstImgLoaded] = useState(false);
+	const [imgsLoaded, setImgsLoaded] = useState(false);
+
+	useEffect(() => {
+		function loadImage(image) {
+			return new Promise((resolve, reject) => {
+				const loadImg = new Image();
+				loadImg.src = image;
+				loadImg.onload = () => resolve(image);
+				loadImg.onerror = (err) => reject(err);
+
+				// This line will only run on the first image, to start rendering the component as fast as possible
+				if (!firstImgLoaded) setFirstImgLoaded(true);
+			});
+		}
+
+		// Then we continue to load all images in the background for smooth slider UX
+		Promise.all(images.map((image) => loadImage(image)))
+			.then(() => setImgsLoaded(true))
+			.catch((err) => console.log('Failed to load images', err));
+	}, []);
 
 	const handlePrevious = () => {
 		if (count === 0) {
@@ -22,10 +45,12 @@ function Slider({ title, images }) {
 		setCount((curr) => curr + 1);
 	};
 
-	return (
+	return !firstImgLoaded ? (
+		<div className="slider" />
+	) : (
 		<div className="slider">
 			<img src={images[count]} alt={title} />
-			{images.length > 1 && (
+			{imgsLoaded && images.length > 1 && (
 				<button
 					onClick={handlePrevious}
 					type="button"
@@ -34,7 +59,7 @@ function Slider({ title, images }) {
 					<img src={leftArrow} alt="Précédent" />
 				</button>
 			)}
-			{images.length > 1 && (
+			{imgsLoaded && images.length > 1 && (
 				<button
 					onClick={handleNext}
 					type="button"
@@ -43,7 +68,7 @@ function Slider({ title, images }) {
 					<img src={rightArrow} alt="Suivant" />
 				</button>
 			)}
-			{images.length > 1 && (
+			{imgsLoaded && images.length > 1 && (
 				<span className="slider__counter">
 					{count + 1}/{images.length}
 				</span>
